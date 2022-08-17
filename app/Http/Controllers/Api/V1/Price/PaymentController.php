69f10d34d04bcc26426fedf7148aab1bed11eb44
SalawatJoldasbaev <jsalawatdeveloper@gmail.com>
2022-08-17 17:06:46 +0500

    bug fixed, payment debt

--- app/Http/Controllers/Api/V1/Price/PaymentController.php
+++ app/Http/Controllers/Api/V1/Price/PaymentController.php
@@ -24,12 +24,12 @@ class PaymentController extends Controller
         $debt = $basket->debt;
         $cost_price = 0;
         $remaining_sum = [
-            'card'=> 0,
-            'cash'=> 0,
+            'card' => 0,
+            'cash' => 0,
         ];
         $paid_sum = $basket->cash + $basket->card + $basket->debt['paid'];
         $sum = $card + $cash;
-        if($sum - $debt['remaining'] > 500){
+        if ($sum - $debt['remaining'] > 500) {
             return ApiResponse::error('There is no extra charge', 409);
         }
         if ($card) {
@@ -37,7 +37,7 @@ class PaymentController extends Controller
             $debt['paid'] += $card;
             if ($debt['remaining'] < 0) {
                 $remaining_sum['card'] = abs($debt['remaining']);
-                $card +=$debt['remaining'];
+                $card += $debt['remaining'];
             }
         }
         if ($cash) {
@@ -66,21 +66,21 @@ class PaymentController extends Controller
         $balance['card'] += $card;
         $balance['cash'] += $cash;
         $balance['sum'] += $sum;
-        PaymentHistory::create([
-            'basket_id' => $basket->id,
-            'employee_id' => $request->user()->id ?? $request->employee_id,
-            'user_id' => $basket->user_id,
-            'amount_paid' => [
-                'card' => $card,
-                'cash' => $cash,
-            ],
-            'paid_time' => Carbon::now(),
-        ]);
-        if(array_sum($remaining_sum) > 0){
+        // PaymentHistory::create([
+        //     'basket_id' => $basket->id,
+        //     'employee_id' => $request->user()->id ?? $request->employee_id,
+        //     'user_id' => $basket->user_id,
+        //     'amount_paid' => [
+        //         'card' => $card,
+        //         'cash' => $cash,
+        //     ],
+        //     'paid_time' => Carbon::now(),
+        // ]);
+        if (array_sum($remaining_sum) > 0) {
             $remaining_basket = Basket::where('user_id', $basket->user_id)->where('debt->remaining', '>', 0)->whereNot('id', $basket->id)->first();
             if ($remaining_basket) {
                 $this->paidDebt(new Request([
-                    'employee_id'=> $request->user()->id,
+                    'employee_id' => $request->user()->id,
                     'basket_id' => $remaining_basket->id,
                     'cash' => $remaining_sum['cash'],
                     'card' => $remaining_sum['card'],
@@ -88,9 +88,9 @@ class PaymentController extends Controller
             }
         }
         $user = $basket->user;
-        $user->balance += $sum - $debt['remaining'];
-        $user->save();
-
+        $user->update([
+            'balance' => $user->balance + $sum
+        ]);
         $profit = $paid_sum - $cost_price;
         $temp_profit = $profit;
 
@@ -169,6 +169,6 @@ class PaymentController extends Controller
                 'paid_time' => $history->paid_time,
             ];
         }
-        return ApiResponse::success(data:$final);
+        return ApiResponse::success(data: $final);
     }
 }
--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -17,40 +17,40 @@ class OrderSeeder extends Seeder
      */
     public function run()
     {
-        $basket = Basket::create([
-            'user_id' => 2,
-            'employee_id' => 1,
-            'card' => 5000,
-            'cash' => 0,
-            'debt' => [
-                'debt' => 10000,
-                'paid' => 5000,
-                'remaining' => 5000,
-            ],
-            'term' => Carbon::today()->add(1, 'day'),
-            'description' => 'qoyishay',
-        ]);
-        $orders = [
-            [
-                'basket_id' => $basket->id,
-                'user_id' => 2,
-                'product_id' => 1,
-                'unit_id' => 1,
-                'count' => 1,
-                'price' => 15000,
-            ],
-        ];
+        // $basket = Basket::create([
+        //     'user_id' => 2,
+        //     'employee_id' => 1,
+        //     'card' => 5000,
+        //     'cash' => 0,
+        //     'debt' => [
+        //         'debt' => 10000,
+        //         'paid' => 5000,
+        //         'remaining' => 5000,
+        //     ],
+        //     'term' => Carbon::today()->add(1, 'day'),
+        //     'description' => 'qoyishay',
+        // ]);
+        // $orders = [
+        //     [
+        //         'basket_id' => $basket->id,
+        //         'user_id' => 2,
+        //         'product_id' => 1,
+        //         'unit_id' => 1,
+        //         'count' => 1,
+        //         'price' => 15000,
+        //     ],
+        // ];
 
-        Order::upsert($orders, [
-            'basket_id',
-            'user_id',
-            'product_id',
-            'unit_id',
-            'count',
-            'price',
-        ]);
-        User::find(2)->update([
-            'balance' => -5000,
-        ]);
+        // Order::upsert($orders, [
+        //     'basket_id',
+        //     'user_id',
+        //     'product_id',
+        //     'unit_id',
+        //     'count',
+        //     'price',
+        // ]);
+        // User::find(2)->update([
+        //     'balance' => -5000,
+        // ]);
     }
 }
