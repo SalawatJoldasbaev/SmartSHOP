85d1a782093b3ef32835610be52bdbd9414bc48d
salawat <jsalawatdeveloper@gmail.com>
2022-10-30 19:27:22 +0500

    front-end added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,20 @@
-<!doctype html><html lang="uz"><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width,initial-scale=1"/><meta name="theme-color" content="#000000"/><meta name="description" content="O'z biznesingizni SmartSHOP bilan boshqaring"/><link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.20.1/antd.min.css" integrity="sha512-GMySjYO0D+1mAzdactAlE4wWMoUYgYpKmVWl1IRAgbUOjn52LwR9RzEEmkZ7QGycJrtwE1nStNYVH3TnUXXAaQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/><title>SmartSHOP</title><script defer="defer" src="/static/js/main.658224fd.js"></script><link href="/static/css/main.6ad99989.css" rel="stylesheet"></head><body><div id="root"></div></body></html>
+<!doctype html>
+<html lang="uz">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="O'z biznesingizni SmartSHOP bilan boshqaring" />
+    <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.20.1/antd.min.css" integrity="sha512-GMySjYO0D+1mAzdactAlE4wWMoUYgYpKmVWl1IRAgbUOjn52LwR9RzEEmkZ7QGycJrtwE1nStNYVH3TnUXXAaQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>SmartSHOP</title>
+    <script defer="defer" src="/static/js/main.7703fcc3.js"></script>
+    <link href="/static/css/main.cf5a8f17.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="root"></div>
+</body>
+
+</html>
